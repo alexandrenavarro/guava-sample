6e46f9a45db2c7de6ab4a2694496f2ecde50ab58
alexandrenavarro <alexandre.j.navarro@gmail.com>
2013-05-08 23:48:08 +0200

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+
+/**
+ * <p>Main. </p>
+ *
+ * @author anavarro - May 8, 2013
+ *
+ */
+public class Main {
+
+    /**
+     * main.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("begin");
+        
+        System.out.println("end");
+    }
+}
